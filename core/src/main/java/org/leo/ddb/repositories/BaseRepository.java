a82dda0bbcbab8faa42b112d2f3dceb75997a849
praveen.govindan@gmail.com <praveen.govindan@gmail.com>
2020-08-01 11:09:18 -0700

    Remove deprecated class

--- core/src/main/java/org/leo/ddb/repositories/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package org.leo.ddb.repositories;
-
-/**
- * @deprecated Present to maintain backward compatibility. Will be removed soon. Please use {@link NonBlockingBaseRepository}
- * @param <T>
- */
-@Deprecated
-public interface BaseRepository<T> extends NonBlockingBaseRepository<T> {
-}
